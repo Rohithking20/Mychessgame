a2eae0ede96dd790c2eed61b7c232c9568275d7a
Rohithking20 <123074722+Rohithking20@users.noreply.github.com>
2024-04-18 23:34:04 +0530

    Update GameEnds.js

--- components/GameEnds.js
+++ components/GameEnds.js
@@ -16,16 +16,11 @@ const GameEnds = ({ onClosePopup }) => {
     const isWin = status.endsWith('wins');
     const winner = isWin ? (status.startsWith('white') ? 'White' : 'Black') : 'Nobody';
     
-    // Display a simple alert message indicating the winner or draw
     window.alert(isWin ? `${winner} wins!` : 'It\'s a draw.');
 
     return (
         <div className="popup--inner popup--inner__center">
-            {/* This part won't be rendered as the alert will take precedence */}
-            {/* <h1>{isWin ? status : 'Draw'}</h1>
-            <p>{!isWin && status}</p>
-            <div className={`${status}`} />
-            <button onClick={newGame}>New Game</button> */}
+            
         </div>
     );
 };
