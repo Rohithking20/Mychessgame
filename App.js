a72e64d232998c57861b9645e0d61e8e561c9a9e
Rohithking20 <123074722+Rohithking20@users.noreply.github.com>
2024-04-18 23:25:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Board from './components/Board';
+import { reducer } from './components/reducer'
+import { useReducer } from 'react'
+import { initGameState } from './components/constants';
+import AppContext from './components/context'
+//import Control from './components/Control';
+//import TakeBack from './components/Control/bits/TakeBack';
+//import MovesList from './components/Control/bits/MovesList';
+
+function App() {
+
+    const [appState, dispatch ] = useReducer(reducer,initGameState);
+
+    const providerState = {
+        appState,
+        dispatch
+    }
+
+    return (
+        <AppContext.Provider value={providerState} >
+            <div className="App">
+                <Board/>
+            </div>
+        </AppContext.Provider>
+    ); 
+}
+
+export default App;
